d33a5daace9577b60f78d88f270a491d9bfbf562
KarenPNavarro <karennavarrofigueroa@protonmail.com>
2026-07-31 13:18:47 -0400

    Fix task 1 concurrent coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,16 +10,15 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     """
-    Spawn wait_random n times with max_delay
-    and return the delays in ascending order.
+    Execute wait_random n times concurrently.
     """
 
-    tasks = []
-
-    for _ in range(n):
-        tasks.append(asyncio.create_task(wait_random(max_delay)))
-
     delays = []
+
+    tasks = [
+        asyncio.create_task(wait_random(max_delay))
+        for _ in range(n)
+    ]
 
     for task in asyncio.as_completed(tasks):
         delay = await task
